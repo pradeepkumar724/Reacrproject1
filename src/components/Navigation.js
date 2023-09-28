f9edd6439269bea1e3f75d9142df7713314d73b1
pradeep <724pradeep@gmail.com>
2023-09-28 18:27:51 +0530

    completed

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,25 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import NikeLogo from "../assets/nikelogo.png";
 import classes from "./Navigation.module.css";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { RxCross2 } from "react-icons/rx";
 
 const Navigation = () => {
+  const [openHamburger, setOpenHamburger] = useState(false);
+  // console.log(openHamburger);
   return (
-    <div>
-      <nav className={classes.nav}>
-        <div>
-          <img src={NikeLogo} alt="nikelogo" className={classes.logo} />
+    <nav className={classes.nav}>
+      <div>
+        <img src={NikeLogo} alt="nikelogo" className={classes.logo} />
+      </div>
+      <ul className={classes.lists}>
+        <li>Menu</li>
+        <li>Location</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+
+      <div>
+        <button className={classes.button}>Login</button>
+
+        <div
+          onClick={() => setOpenHamburger((prev) => !prev)}
+          className={classes.menuContainer}
+        >
+          <span className={classes.menu}>
+            <HiMenuAlt1 />
+          </span>
         </div>
-        <ul className={classes.lists}>
+      </div>
+
+      <div
+        className={`${
+          openHamburger ? classes.hamburgerMobile : classes.hamburger
+        }`}
+      >
+        <ul className={classes.menuOptions}>
           <li>Menu</li>
           <li>Location</li>
           <li>About</li>
           <li>Contact</li>
         </ul>
-        <div>
-          <button className={classes.button}>Login</button>
-        </div>
-      </nav>
-    </div>
+        <span
+          className={classes.menuCross}
+          onClick={() => setOpenHamburger((prev) => !prev)}
+        >
+          <RxCross2 />
+        </span>
+      </div>
+    </nav>
   );
 };
 
